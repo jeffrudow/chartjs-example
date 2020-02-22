83102676e8f2e79a7b0d2a97fa442045a7d87ac3
Jeff Rudow <jeffrudow@gmail.com>
2020-02-22 14:51:20 -0500

    Add second dataset

--- src/LineDemo.js
+++ src/LineDemo.js
@@ -24,6 +24,27 @@ const data = {
       pointRadius: 1,
       pointHitRadius: 10,
       data: randomInts(7, 100)
+    },
+    {
+      label: "My Second dataset",
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: "rgba(204, 140, 255, .4)",
+      borderColor: "rgba(204, 140, 255, 1)",
+      borderCapStyle: "butt",
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: "miter",
+      pointBorderColor: "rgba(204, 140, 255, 1)",
+      pointBackgroundColor: "#fff",
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(204, 140, 255, 1)",
+      pointHoverBorderColor: "rgba(220,220,220,1)",
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: randomInts(7, 100)
     }
   ]
 };
