4ae5315f7fb74ed75e71a82584e4c36b900325c0
Jeff Rudow <jeffrudow@gmail.com>
2020-02-22 09:52:05 -0500

    Add chartjs example

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import LineDemo from './LineDemo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => (
+  <div style={styles}>
+    <LineDemo/>
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
