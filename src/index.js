f6c04ec6c356155a405d1e30ca5e5ca89baf5d25
Jeff Rudow <jeffrudow@gmail.com>
2020-02-22 14:42:13 -0500

    Add random dataset

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { render } from 'react-dom';
-import LineDemo from './LineDemo';
+import React from "react";
+import { render } from "react-dom";
+import LineDemo from "./LineDemo";
 
 const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
+  fontFamily: "sans-serif",
+  textAlign: "center",
+  outerWidth: "200px"
 };
 
 const App = () => (
   <div style={styles}>
-    <LineDemo/>
+    <LineDemo />
   </div>
 );
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
